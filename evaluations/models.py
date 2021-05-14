b501c57b4bfb5cc7ef15940d2046c536248d970d
Amy Gori <amydgori@gmail.com>
2021-05-13 22:22:52 -0400

    Add skill category

--- evaluations/models.py
+++ evaluations/models.py
@@ -10,11 +10,24 @@ User = get_user_model()
 
 
 class Skill(SortableMixin):
+    class Category(models.TextChoices):
+        FRONT_END = 'fe', 'Front End'
+        BACK_END = 'be', 'Back End'
+        DEV_TOOLS = 'dt', 'Dev Tools'
+        AGILE = 'ag', 'Agile Development'
+        SOFT_SKILLS = 'ss', 'Soft Skills'
+
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True, null=True)
     levels = ArrayField(base_field=models.CharField(max_length=500), size=4)
-    order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+    order = models.PositiveIntegerField(default=0,
+                                        editable=False,
+                                        db_index=True)
     version = models.PositiveIntegerField(default=1)
+    category = models.CharField(max_length=2,
+                                choices=Category.choices,
+                                null=True,
+                                blank=True)
 
     class Meta:
         ordering = ["order"]
@@ -24,20 +37,20 @@ class Skill(SortableMixin):
 
 
 class ScheduledEvaluation(models.Model):
-    team = models.ForeignKey(
-        to=Team, on_delete=models.PROTECT, related_name="scheduled_evaluations"
-    )
+    team = models.ForeignKey(to=Team,
+                             on_delete=models.PROTECT,
+                             related_name="scheduled_evaluations")
     start_date = models.DateField()
     skills = models.ManyToManyField(to=Skill, related_name="+")
 
 
 class Evaluation(models.Model):
-    scheduled_by = models.ForeignKey(
-        to=ScheduledEvaluation, related_name="evaluations", on_delete=models.PROTECT
-    )
-    user = models.ForeignKey(
-        to=User, related_name="evaluations", on_delete=models.PROTECT
-    )
+    scheduled_by = models.ForeignKey(to=ScheduledEvaluation,
+                                     related_name="evaluations",
+                                     on_delete=models.PROTECT)
+    user = models.ForeignKey(to=User,
+                             related_name="evaluations",
+                             on_delete=models.PROTECT)
     evaluated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -45,12 +58,12 @@ class Evaluation(models.Model):
 
 
 class SkillEvaluation(models.Model):
-    evaluation = models.ForeignKey(
-        to=Evaluation, related_name="skill_evaluations", on_delete=models.CASCADE
-    )
-    skill = models.ForeignKey(
-        to=Skill, related_name="skill_evaluations", on_delete=models.PROTECT
-    )
+    evaluation = models.ForeignKey(to=Evaluation,
+                                   related_name="skill_evaluations",
+                                   on_delete=models.CASCADE)
+    skill = models.ForeignKey(to=Skill,
+                              related_name="skill_evaluations",
+                              on_delete=models.PROTECT)
     score = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(4)]
-    )
+        validators=[MinValueValidator(0),
+                    MaxValueValidator(4)])
