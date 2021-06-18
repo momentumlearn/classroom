6c2b764eb727e20596809d8d51ccfe2f6d8f0abd
Amy Gori <amydgori@gmail.com>
2021-06-18 17:42:13 -0400

    Add button to check all skills for scheduled evaluation

--- evaluations/forms.py
+++ evaluations/forms.py
@@ -1,8 +1,8 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Layout, HTML, Fieldset
 from django import forms
 
-from .models import Evaluation, ScheduledEvaluation, SkillEvaluation, Skill
+from .models import Evaluation, ScheduledEvaluation, Skill
 
 
 class BetterDateInput(forms.DateInput):
@@ -13,8 +13,7 @@ class ScheduledEvaluationForm(forms.ModelForm):
     skills = forms.ModelMultipleChoiceField(
         required=False,
         widget=forms.CheckboxSelectMultiple,
-        queryset=Skill.objects.filter(version=2)
-    )
+        queryset=Skill.objects.filter(version=2))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -22,11 +21,18 @@ class ScheduledEvaluationForm(forms.ModelForm):
 
         self.helper.form_method = "post"
         self.helper.form_action = "schedule_evaluation"
-
-        self.helper.add_input(
-            Submit("submit", "Schedule evaluation",
-                   css_class="submit-button grow")
+        self.helper.layout = Layout(
+            "team",
+            "start_date",
+            HTML(
+                '<button type="button" id="check-all" class="f6 link ph3 pv2 mb2 dib white b btn--select-all">Check All</button>'
+            ),
+            "skills",
         )
+        self.helper.add_input(
+            Submit("submit",
+                   "Schedule evaluation",
+                   css_class="submit-button grow mt2"))
 
     class Meta:
         model = ScheduledEvaluation
@@ -50,12 +56,8 @@ class EvaluationForm(forms.Form):
             self.fields[f"skill_{skill.pk}"] = forms.TypedChoiceField(
                 label=skill.name,
                 required=True,
-                choices=(
-                    [
-                        (i, f"{i}. {level}")
-                        for i, level in enumerate(skill.levels)
-                    ]
-                ),
+                choices=([(i, f"{i}. {level}")
+                          for i, level in enumerate(skill.levels)]),
                 widget=forms.RadioSelect,
             )
 
@@ -64,9 +66,9 @@ class EvaluationForm(forms.Form):
         self.helper.form_method = "post"
 
         self.helper.add_input(
-            Submit("submit", "Submit evaluation",
-                   css_class="submit-button grow")
-        )
+            Submit("submit",
+                   "Submit evaluation",
+                   css_class="submit-button grow"))
 
     def save(self, user):
         if not self.is_valid():
@@ -77,7 +79,6 @@ class EvaluationForm(forms.Form):
 
         for skill in self.scheduled_eval.skills.all():
             evaluation.skill_evaluations.create(
-                skill=skill, score=self.cleaned_data[f"skill_{skill.pk}"]
-            )
+                skill=skill, score=self.cleaned_data[f"skill_{skill.pk}"])
 
         return evaluation
--- static/js/schedule_evaluation.js
+++ static/js/schedule_evaluation.js
@@ -1,6 +1,6 @@
 const skills = document.querySelector("#div_id_skills")
 all_skills = skills.querySelectorAll("input")
-const selectAllBtn = document.querySelector("#select-all")
+const selectAllBtn = document.querySelector("#check-all")
 
 const toggleSkills = () => {
   selectAllSkills()
@@ -9,8 +9,9 @@ const toggleSkills = () => {
 
 const toggleButtonText = () => {
   text = selectAllBtn.innerText
-  selectAllBtn.innerText = text == 'Check All' ? 'Uncheck all' : 'Check all'
+  selectAllBtn.innerText = text == 'Check All' ? 'Uncheck All' : 'Check All'
 }
+
 const selectAllSkills = () => all_skills.forEach(skill => skill.toggleAttribute('checked'))
 
 selectAllBtn.addEventListener("click", toggleSkills)
